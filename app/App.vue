ed548e9d3ef6eb11b2e57ca31d5d211365094a2a
tiffany6022 <tiffany001510@gmail.com>
2019-07-01 21:01:42 +0800

    tiffany's todolist

---
+++ app/App.vue
@@ -0,0 +1,65 @@
+<template lang="pug">
+#app
+
+  #input.ui.input.action
+    input(type="text", v-model='todo', placeholder="add todo...")
+    button.ui.icon.button(@click='addTask()')
+      i.plus.icon
+
+  .task(v-for="task in tasks")
+    .ui.checkbox(@click='toggleTask(task.name)')
+      input(type="checkbox" v-model='task.toggle')
+      label(v-if='!task.toggle') {{task.name}}
+      label(v-if='task.toggle')
+        del {{task.name}}
+    i.delete.icon(v-if='task.toggle' @click='rmTask(task.name)')
+
+</template>
+
+<script>
+export default {
+
+  data() {return {
+    todo: 'Data binding is cool!',
+    tasks: []
+  }},
+
+  methods: {
+
+    addTask(){ 
+      if (!this.todo)
+        return
+      this.tasks.push({
+        name: this.todo,
+        toggle: false
+      })
+      this.todo = ''
+    },
+
+    rmTask(tsk) {
+      this.tasks = this.tasks.filter(task => task.name != tsk)
+    },
+
+    toggleTask(tsk) {
+      const toggle = this.tasks.find(task => task.name === tsk).toggle
+      this.tasks.find(task => task.name === tsk).toggle = !toggle
+
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+html
+  font-size: large
+#app
+  width: 22em
+  margin: auto
+  display: flex
+  flex-direction: column
+  padding: 2em
+.task
+  margin-top: 1em
+  display: flex
+  justify-content: space-between
+</style>
---
+++ app/app.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import 'semantic-ui-offline/semantic.min.css'
+import App from './App.vue'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
+
+  
